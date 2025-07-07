3889566b99adf059c94cb53ef14f0cc4f65cea08
i7C-40 <i7C-40@DESKTOP-4JEE2Q3>
2025-07-07 09:52:41 +0900

    작업시작

--- src/ezen/market/gui/MainConsole.java
+++ src/ezen/market/gui/MainConsole.java
@@ -1,8 +1,15 @@
 package ezen.market.gui;
 
+import java.util.Scanner;
+
 public class MainConsole extends Thread {
+	Scanner sc = new Scanner(System.in);
+	
 	@Override
 	public void run() {
-
+		while (true) {
+			System.out.println("> ");
+			sc.nextLine();
+		}
 	}
 }
